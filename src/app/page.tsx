868e8e35fa8272b0df9fbffe24383fd13d7db8a9
Johnny Gérard <john@jgerard.dev>
2025-03-03 15:33:37 +0100

    feat: add homepage heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import { cn } from "@/util/cn";
 import { memo } from "react";
 
 const HomePage = () => {
-  return null;
+  return (
+    <>
+      <h1
+        className={cn(
+          "tv_display mt-14 dark:text-neutral-100",
+          "text-center whitespace-pre-line",
+        )}
+      >
+        Analyze your text{"\n"}in real&#x2011;time.
+      </h1>
+    </>
+  );
 };
 
 export default memo(HomePage);
