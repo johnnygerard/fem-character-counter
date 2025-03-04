892f38601a4acff507e1138f64b06ad5c42b8c8d
Johnny Gérard <john@jgerard.dev>
2025-03-04 12:01:06 +0100

    feat: add text-counters.tsx

--- src/component/text-analyzer.tsx
+++ src/component/text-analyzer.tsx
@@ -1,11 +1,17 @@
 "use client";
 import AppTextarea from "@/component/app-textarea";
+import TextCounters from "@/component/text-counters";
 import { memo, useState } from "react";
 
 const TextAnalyzer = () => {
   const [text, setText] = useState("");
 
-  return <AppTextarea text={text} setText={setText} />;
+  return (
+    <>
+      <AppTextarea text={text} setText={setText} />
+      <TextCounters className="mt-10 dt:mt-12" text={text} />
+    </>
+  );
 };
 
 export default memo(TextAnalyzer);
---
+++ src/component/text-counters.tsx
@@ -0,0 +1,63 @@
+import ShapeCharacterCount from "@/component/svg/shape-character-count";
+import ShapeSentenceCount from "@/component/svg/shape-sentence-count";
+import ShapeWordCount from "@/component/svg/shape-word-count";
+import { cn } from "@/util/cn";
+import { countCharacters } from "@/util/count-characters";
+import { countSentences } from "@/util/count-sentences";
+import { countWords } from "@/util/count-words";
+import { memo } from "react";
+
+type Props = {
+  className: string;
+  text: string;
+};
+
+const TextCounters = ({ className, text }: Props) => {
+  const counters = [
+    {
+      count: countCharacters(text),
+      name: "Total Characters",
+      Shape: ShapeCharacterCount,
+    },
+    {
+      count: countWords(text),
+      name: "Word Count",
+      Shape: ShapeWordCount,
+    },
+    {
+      count: countSentences(text),
+      name: "Sentence Count",
+      Shape: ShapeSentenceCount,
+    },
+  ];
+
+  return (
+    <div className={cn("flex flex-col gap-4 tb:flex-row", className)}>
+      {counters.map(({ count, name, Shape }, index) => (
+        <div
+          key={name}
+          className={cn(
+            ["bg-purple-400", "bg-yellow-500", "bg-orange-500"][index],
+            "h-32.5 rounded-12 tb:h-37.5 tb:flex-1",
+            "p-5 tb:px-3 tb:py-4 dt:px-4",
+            "flex flex-col justify-center gap-2 dt:gap-1.25",
+            "relative overflow-hidden text-neutral-900",
+          )}
+        >
+          <span className="tv_display">
+            {count.toString(10).padStart(2, "0")}
+          </span>
+          {name}
+          <Shape
+            className={cn(
+              "-right-[3.15625rem] tb:-right-[4.40625rem] dt:-right-[1.88544rem]",
+              "absolute h-37.5 w-37.5",
+            )}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default memo(TextCounters);
