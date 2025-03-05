2bfeafa1c4e1147846a154672552d2fc6811eec6
Johnny Gérard <john@jgerard.dev>
2025-03-05 04:46:01 +0100

    feat: exclude whitespace instead of single spaces

--- src/util/count-characters.ts
+++ src/util/count-characters.ts
@@ -1,11 +1,17 @@
-export const countCharacters = (text: string, withSpaces = true): number => {
+export const countCharacters = (
+  text: string,
+  isSpaceExcluded = false,
+): number => {
   let count = 0;
 
-  if (withSpaces) {
+  if (isSpaceExcluded) {
+    for (const char of text) {
+      if (/\s/.test(char)) continue;
+      count++;
+    }
+  } else {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     for (const _ of text) count++;
-  } else {
-    for (const char of text) if (char !== " ") count++;
   }
 
   return count;
