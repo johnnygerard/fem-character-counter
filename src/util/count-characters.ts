e91e1aaaf9de15b21aaabca3377bc7a1e622f002
Johnny Gérard <john@jgerard.dev>
2025-03-05 06:01:54 +0100

    feat: use grapheme segmenter for count-characters.ts

--- src/util/count-characters.ts
+++ src/util/count-characters.ts
@@ -1,17 +1,20 @@
+const segmenter = new Intl.Segmenter("en-US", { granularity: "grapheme" });
+
 export const countCharacters = (
   text: string,
   isSpaceExcluded = false,
 ): number => {
+  const segments = segmenter.segment(text);
   let count = 0;
 
   if (isSpaceExcluded) {
-    for (const char of text) {
-      if (/\s/.test(char)) continue;
+    for (const { segment } of segments) {
+      if (/\s/.test(segment)) continue;
       count++;
     }
   } else {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    for (const _ of text) count++;
+    for (const _ of segments) count++;
   }
 
   return count;
